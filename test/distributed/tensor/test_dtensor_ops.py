afe0ba4348e8f3108254543517c61bfe4905143f
Pian Pawakapan <pianpwk@meta.com>
2026-01-15 13:15:23 -0800

    opinfo unbacked tets

--- test/distributed/tensor/test_dtensor_ops.py
+++ test/distributed/tensor/test_dtensor_ops.py
@@ -7,6 +7,7 @@ import unittest
 import warnings
 
 import torch
+import torch._dynamo
 import torch.distributed as dist
 import torch.testing._internal.common_methods_invocations as common_ops
 from torch.distributed._local_tensor import LocalTensorMode, reconcile_args
@@ -17,6 +18,7 @@ from torch.distributed.tensor import (
     Replicate,
     Shard,
 )
+from torch.fx.experimental.symbolic_shapes import GuardOnDataDependentSymNode
 from torch.overrides import resolve_name
 from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests,
@@ -30,7 +32,7 @@ from torch.testing._internal.distributed._tensor.common_dtensor import (
 )
 from torch.utils import _pytree as pytree
 from torch.utils._debug_mode import DebugMode
-from torch.utils._pytree import tree_map
+from torch.utils._pytree import tree_flatten, tree_map
 
 
 # rewrite common size variables to sth can be sharded evenly
@@ -806,12 +808,470 @@ class TestLocalDTensorOps(TestDTensorOps):
         self.assertEqual(x, y, msg)
 
 
+# Ops where base tensor has DDEs with unbacked (not DTensor's fault)
+ops_unbacked_base_dde = {
+    xfail("_chunk_cat"),
+    xfail("_unsafe_masked_index_put_accumulate"),
+    xfail("_upsample_bilinear2d_aa"),
+    xfail("addmv"),
+    xfail("allclose"),
+    xfail("as_strided_scatter"),
+    xfail("baddbmm"),
+    xfail("bernoulli"),
+    xfail("cauchy"),
+    xfail("cdist"),
+    xfail("cholesky"),
+    xfail("cholesky_inverse"),
+    xfail("chunk"),
+    xfail("combinations"),
+    xfail("corrcoef"),
+    xfail("cov"),
+    xfail("cross"),
+    xfail("cummax"),
+    xfail("cummin"),
+    xfail("cumulative_trapezoid"),
+    xfail("diag"),
+    xfail("diagonal"),
+    xfail("diagonal_copy"),
+    xfail("diagonal_scatter"),
+    xfail("diff"),
+    xfail("dist"),
+    xfail("dsplit"),
+    xfail("equal"),
+    xfail("exponential"),
+    xfail("fft.fft"),
+    xfail("fft.fft2"),
+    xfail("fft.fftn"),
+    xfail("fft.fftshift"),
+    xfail("fft.hfft"),
+    xfail("fft.hfft2"),
+    xfail("fft.hfftn"),
+    xfail("fft.ifft"),
+    xfail("fft.ifft2"),
+    xfail("fft.ifftn"),
+    xfail("fft.ifftshift"),
+    xfail("fft.ihfft"),
+    xfail("fft.ihfft2"),
+    xfail("fft.ihfftn"),
+    xfail("fft.irfft"),
+    xfail("fft.irfft2"),
+    xfail("fft.irfftn"),
+    xfail("fft.rfft"),
+    xfail("fft.rfft2"),
+    xfail("fft.rfftn"),
+    xfail("float"),
+    xfail("geometric"),
+    xfail("geqrf"),
+    xfail("gradient"),
+    xfail("grid_sampler_2d"),
+    xfail("hash_tensor"),
+    xfail("histogram"),
+    xfail("histogramdd"),
+    xfail("hsplit"),
+    xfail("index_fill"),
+    xfail("inner"),
+    xfail("kron"),
+    xfail("linalg.cross"),
+    xfail("linalg.cholesky"),
+    xfail("linalg.cholesky_ex"),
+    xfail("linalg.cond"),
+    xfail("linalg.det"),
+    xfail("linalg.diagonal"),
+    xfail("linalg.eig"),
+    xfail("linalg.eigh"),
+    xfail("linalg.eigvals"),
+    xfail("linalg.eigvalsh"),
+    xfail("linalg.householder_product"),
+    xfail("linalg.inv"),
+    xfail("linalg.inv_ex"),
+    xfail("linalg.ldl_factor"),
+    xfail("linalg.ldl_factor_ex"),
+    xfail("linalg.ldl_solve"),
+    xfail("linalg.lstsq"),
+    xfail("linalg.lstsq", "grad_oriented"),
+    xfail("linalg.lu"),
+    xfail("linalg.lu_factor"),
+    xfail("linalg.lu_factor_ex"),
+    xfail("linalg.lu_solve"),
+    xfail("linalg.matrix_norm"),
+    xfail("linalg.matrix_power"),
+    xfail("linalg.matrix_rank"),
+    xfail("linalg.matrix_rank", "hermitian"),
+    xfail("linalg.multi_dot"),
+    xfail("linalg.norm"),
+    xfail("linalg.norm", "subgradients_at_zero"),
+    xfail("linalg.pinv"),
+    xfail("linalg.pinv", "hermitian"),
+    xfail("linalg.pinv", "singular"),
+    xfail("linalg.qr"),
+    xfail("linalg.slogdet"),
+    xfail("linalg.solve"),
+    xfail("linalg.solve_ex"),
+    xfail("linalg.solve_triangular"),
+    xfail("linalg.svd"),
+    xfail("linalg.svdvals"),
+    xfail("linalg.tensorinv"),
+    xfail("linalg.tensorsolve"),
+    xfail("linalg.vander"),
+    xfail("linalg.vector_norm"),
+    xfail("log_normal"),
+    xfail("logdet"),
+    xfail("logsumexp"),
+    xfail("lu"),
+    xfail("lu_solve"),
+    xfail("lu_unpack"),
+    xfail("masked.amax"),
+    xfail("masked.amin"),
+    xfail("masked.argmax"),
+    xfail("masked.argmin"),
+    xfail("masked.cumprod"),
+    xfail("masked.cumsum"),
+    xfail("masked.log_softmax"),
+    xfail("masked.logaddexp"),
+    xfail("masked.logsumexp"),
+    xfail("masked.mean"),
+    xfail("masked.median"),
+    xfail("masked.norm"),
+    xfail("masked.prod"),
+    xfail("masked.softmax"),
+    xfail("masked.softmin"),
+    xfail("masked.std"),
+    xfail("masked.sum"),
+    xfail("masked.var"),
+    xfail("matrix_exp"),
+    xfail("max_pool2d_with_indices_backward"),
+    xfail("multinomial"),
+    xfail("nanquantile"),
+    xfail("nn.functional.adaptive_avg_pool1d"),
+    xfail("nn.functional.adaptive_avg_pool2d"),
+    xfail("nn.functional.adaptive_avg_pool3d"),
+    xfail("nn.functional.adaptive_max_pool1d"),
+    xfail("nn.functional.adaptive_max_pool2d"),
+    xfail("nn.functional.adaptive_max_pool3d"),
+    xfail("nn.functional.alpha_dropout"),
+    xfail("nn.functional.avg_pool1d"),
+    xfail("nn.functional.avg_pool2d"),
+    xfail("nn.functional.avg_pool3d"),
+    xfail("nn.functional.batch_norm"),
+    xfail("nn.functional.bilinear"),
+    xfail("nn.functional.binary_cross_entropy"),
+    xfail("nn.functional.binary_cross_entropy_with_logits"),
+    xfail("nn.functional.channel_shuffle"),
+    xfail("nn.functional.conv1d"),
+    xfail("nn.functional.conv2d"),
+    xfail("nn.functional.conv3d"),
+    xfail("nn.functional.conv_transpose1d"),
+    xfail("nn.functional.conv_transpose2d"),
+    xfail("nn.functional.conv_transpose3d"),
+    xfail("nn.functional.cosine_similarity"),
+    xfail("nn.functional.cross_entropy"),
+    xfail("nn.functional.ctc_loss"),
+    xfail("nn.functional.dropout"),
+    xfail("nn.functional.dropout2d"),
+    xfail("nn.functional.dropout3d"),
+    xfail("nn.functional.embedding"),
+    xfail("nn.functional.embedding_bag"),
+    xfail("nn.functional.feature_alpha_dropout", "with_train"),
+    xfail("nn.functional.feature_alpha_dropout", "without_train"),
+    xfail("nn.functional.fractional_max_pool2d"),
+    xfail("nn.functional.fractional_max_pool3d"),
+    xfail("nn.functional.gaussian_nll_loss"),
+    xfail("nn.functional.glu"),
+    xfail("nn.functional.grid_sample"),
+    xfail("nn.functional.group_norm"),
+    xfail("nn.functional.huber_loss"),
+    xfail("nn.functional.instance_norm"),
+    xfail("nn.functional.interpolate", "area"),
+    xfail("nn.functional.interpolate", "bicubic"),
+    xfail("nn.functional.interpolate", "bilinear"),
+    xfail("nn.functional.interpolate", "linear"),
+    xfail("nn.functional.interpolate", "trilinear"),
+    xfail("nn.functional.l1_loss"),
+    xfail("nn.functional.local_response_norm"),
+    xfail("nn.functional.max_pool1d"),
+    xfail("nn.functional.max_pool2d"),
+    xfail("nn.functional.max_pool3d"),
+    xfail("nn.functional.max_unpool1d"),
+    xfail("nn.functional.max_unpool1d", "grad"),
+    xfail("nn.functional.max_unpool2d"),
+    xfail("nn.functional.max_unpool2d", "grad"),
+    xfail("nn.functional.max_unpool3d"),
+    xfail("nn.functional.max_unpool3d", "grad"),
+    xfail("nn.functional.mse_loss"),
+    xfail("nn.functional.multi_head_attention_forward"),
+    xfail("nn.functional.multilabel_margin_loss"),
+    xfail("nn.functional.nll_loss"),
+    xfail("nn.functional.pad", "circular"),
+    xfail("nn.functional.pad", "reflect"),
+    xfail("nn.functional.pad", "replicate"),
+    xfail("nn.functional.pad", "replicate_negative"),
+    xfail("nn.functional.pdist"),
+    xfail("nn.functional.pixel_shuffle"),
+    xfail("nn.functional.prelu"),
+    xfail("nn.functional.rrelu"),
+    xfail("nn.functional.scaled_dot_product_attention"),
+    xfail("nn.functional.smooth_l1_loss"),
+    xfail("nn.functional.unfold"),
+    xfail("nn.functional.upsample_bilinear"),
+    xfail("norm"),
+    xfail("norm", "fro"),
+    xfail("norm", "nuc"),
+    xfail("normal"),
+    xfail("normal", "in_place"),
+    xfail("normal", "number_mean"),
+    xfail("ormqr"),
+    xfail("pca_lowrank"),
+    xfail("pinverse"),
+    xfail("quantile"),
+    xfail("qr"),
+    xfail("rand_like"),
+    xfail("randint_like"),
+    xfail("randn_like"),
+    xfail("repeat_interleave"),
+    xfail("resize_"),
+    xfail("resize_as_"),
+    xfail("roll"),
+    xfail("scatter"),
+    xfail("scatter_add"),
+    xfail("scatter_reduce", "amax"),
+    xfail("scatter_reduce", "amin"),
+    xfail("scatter_reduce", "mean"),
+    xfail("scatter_reduce", "prod"),
+    xfail("scatter_reduce", "sum"),
+    xfail("searchsorted"),
+    xfail("sparse.mm", "reduce"),
+    xfail("split"),
+    xfail("stft"),
+    xfail("svd"),
+    xfail("svd_lowrank"),
+    xfail("sum_to_size"),
+    xfail("take"),
+    xfail("take_along_dim"),
+    xfail("tensordot"),
+    xfail("tensor_split"),
+    xfail("to_sparse"),
+    xfail("trace"),
+    xfail("trapezoid"),
+    xfail("trapz"),
+    xfail("triangular_solve"),
+    xfail("unbind"),
+    xfail("unbind_copy"),
+    xfail("uniform"),
+    xfail("unsafe_chunk"),
+    xfail("unsafe_split"),
+    xfail("view_as_complex"),
+    xfail("vsplit"),
+}
+
+# Ops where DTensor shard prop has DDEs with unbacked (base tensor passes)
+ops_unbacked_dtensor_dde = {
+    xfail("as_strided"),
+    xfail("as_strided", "partial_views"),
+    xfail("broadcast_tensors"),
+    xfail("cartesian_prod"),
+    xfail("diagflat"),
+    xfail("expand_as"),
+    xfail("gather"),
+    xfail("masked.normalize"),
+    xfail("meshgrid", "list_of_tensors"),
+    xfail("meshgrid", "variadic_tensors"),
+    xfail("new_empty"),
+    xfail("new_empty_strided"),
+    xfail("new_full"),
+    xfail("new_ones"),
+    xfail("new_zeros"),
+    xfail("nn.functional.multi_margin_loss"),
+    xfail("nn.functional.normalize"),
+    xfail("outer"),
+    xfail("ravel"),
+    xfail("reshape_as"),
+    xfail("squeeze"),
+    xfail("squeeze", "multiple"),
+    xfail("topk"),
+    xfail("view_as"),
+}
+
+# Ops that skip for unbacked (no valid sample with markable dims)
+ops_unbacked_skip = {
+    skip("arange"),
+    skip("broadcast_shapes"),
+    skip("empty"),
+    skip("empty_permuted"),
+    skip("empty_strided"),
+    skip("eye"),
+    skip("full"),
+    skip("item"),
+    skip("linspace"),
+    skip("linspace", "tensor_overload"),
+    skip("logspace"),
+    skip("logspace", "tensor_overload"),
+    skip("ones"),
+    skip("randint"),
+    skip("randn"),
+    skip("scalar_tensor"),
+    skip("signal.windows.bartlett"),
+    skip("signal.windows.blackman"),
+    skip("signal.windows.cosine"),
+    skip("signal.windows.exponential"),
+    skip("signal.windows.gaussian"),
+    skip("signal.windows.general_cosine"),
+    skip("signal.windows.general_hamming"),
+    skip("signal.windows.hamming"),
+    skip("signal.windows.hann"),
+    skip("signal.windows.kaiser"),
+    skip("signal.windows.nuttall"),
+    skip("zeros"),
+    # Sparse ops that can't be deepcopied
+    skip("sparse.mm", "reduce"),
+    skip("sparse.sampled_addmm"),
+}
+
+
+class TestUnbackedDTensorOps(TestDTensorOps):
+    """
+    Test suite for DTensor ops with unbacked symints.
+
+    This test marks tensor dimensions as unbacked and verifies that ops
+    can be compiled with fullgraph=True without data-dependent errors.
+    """
+
+    _op_db = repurpose_ops(op_db, "TestDTensorOps", "TestUnbackedDTensorOps")
+
+    def setUp(self) -> None:
+        super().setUp()
+        dist.init_process_group("fake", rank=0, world_size=self.world_size)
+        self.mesh = init_device_mesh(DEVICE_TYPE, (self.world_size,))
+
+    def tearDown(self):
+        super().tearDown()
+        try:
+            dist.destroy_process_group()
+        except AssertionError:
+            pass
+
+    def assertEqualOnRank(self, x, y, msg=None, *, rank=0):
+        self.assertEqual(x, y, msg)
+
+    def _has_valid_unbacked_dims(self, t: torch.Tensor) -> bool:
+        """Check if tensor has dimensions that can be marked as unbacked."""
+        return t.ndim > 0 and any(s >= 2 for s in t.shape)
+
+    def _mark_unbacked(self, t: torch.Tensor) -> None:
+        """Mark all eligible dimensions of a tensor as unbacked."""
+        for i in range(t.ndim):
+            if t.shape[i] >= 2:
+                torch._dynamo.decorators.mark_unbacked(t, i)
+
+    def _check_base_dde(self, func, args, kwargs):
+        """Check if base tensor (without DTensor) has DDEs."""
+        torch._dynamo.reset()
+
+        def mark_unbacked_tree(x):
+            if isinstance(x, torch.Tensor) and self._has_valid_unbacked_dims(x):
+                self._mark_unbacked(x)
+            return x
+
+        tree_map(mark_unbacked_tree, args)
+        tree_map(mark_unbacked_tree, kwargs)
+
+        @torch.compile(backend="eager", fullgraph=True)
+        def compiled_func(*a, **kw):
+            return func(*a, **kw)
+
+        try:
+            compiled_func(*args, **kwargs)
+            return False, None
+        except GuardOnDataDependentSymNode as e:
+            return True, str(e)[:200]
+        except Exception as e:
+            err_str = str(e)
+            if "Could not guard on data-dependent" in err_str or "Could not extract" in err_str:
+                return True, err_str[:200]
+            return False, None
+
+    def _run_unbacked_dtensor_test(self, func, args, kwargs):
+        """Run DTensor op with unbacked dims and fullgraph compile."""
+        torch._dynamo.reset()
+        dtc = DTensorConverter(self.mesh, args, kwargs)
+
+        for d_args, d_kwargs in dtc:
+            if not dtc.successful():
+                continue
+
+            def mark_unbacked_tree(x):
+                if isinstance(x, DTensor) and self._has_valid_unbacked_dims(x):
+                    self._mark_unbacked(x)
+                return x
+
+            tree_map(mark_unbacked_tree, d_args)
+            tree_map(mark_unbacked_tree, d_kwargs)
+
+            @torch.compile(backend="eager", fullgraph=True)
+            def compiled_func(*a, **kw):
+                return func(*a, **kw)
+
+            compiled_func(*d_args, **d_kwargs)
+            return True  # Successfully ran
+
+        return False  # No valid dtensor conversion
+
+    @suppress_warnings
+    @ops(_op_db, allowed_dtypes=(torch.float,))
+    @skipOps(
+        _op_db,
+        "TestUnbackedDTensorOps",
+        "test_unbacked_dtensor_op_db",
+        dtensor_fails | ops_unbacked_base_dde | ops_unbacked_dtensor_dde | ops_unbacked_skip,
+    )
+    def test_unbacked_dtensor_op_db(self, dtype, op):
+        samples = list(op.sample_inputs(DEVICE_TYPE, dtype, requires_grad=False))
+
+        any_tested = False
+
+        for sample in samples:
+            args = [sample.input] + list(sample.args)
+            kwargs = sample.kwargs
+            all_tensors = [
+                x
+                for x in tree_flatten((args, kwargs))[0]
+                if isinstance(x, torch.Tensor)
+            ]
+            if not any(self._has_valid_unbacked_dims(t) for t in all_tensors):
+                continue
+
+            try:
+                args_copy = copy.deepcopy(args)
+                kwargs_copy = copy.deepcopy(kwargs)
+            except NotImplementedError:
+                # Skip samples that can't be deepcopied (e.g., sparse tensors)
+                continue
+
+            any_tested = True
+
+            # First check if base tensor has DDEs (skip DTensor test if so)
+            is_base_dde, _ = self._check_base_dde(op.op, args_copy, kwargs_copy)
+            if is_base_dde:
+                self.fail(f"{op.name}: base tensor has DDE, should be in ops_unbacked_base_dde")
+
+            # Now run the DTensor test with unbacked dims
+            args_copy2 = copy.deepcopy(args)
+            kwargs_copy2 = copy.deepcopy(kwargs)
+            success = self._run_unbacked_dtensor_test(op.op, args_copy2, kwargs_copy2)
+            if success:
+                return  # At least one sample passed
+
+        if not any_tested:
+            self.fail(f"{op.name}: no valid samples found, should be in ops_unbacked_skip")
+
+
 # only instantiate tests for DEVICE_TYPE alone (i.e. either CPU or GPU)
 instantiate_device_type_tests(
     TestMultiThreadedDTensorOps, globals(), only_for=(DEVICE_TYPE,)
 )
 
 instantiate_device_type_tests(TestLocalDTensorOps, globals(), only_for=(DEVICE_TYPE,))
+
+instantiate_device_type_tests(TestUnbackedDTensorOps, globals(), only_for=(DEVICE_TYPE,))
 
 if __name__ == "__main__":
     run_tests()
